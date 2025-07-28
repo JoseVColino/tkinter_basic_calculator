51efdea2087cb0fba28a2b81929a0a869d01bda7
JoseVColino <josevictorcolino@gmail.com>
2025-07-27 22:49:17 -0300

    feat: max atempts

--- number_guess.py
+++ number_guess.py
@@ -1,9 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
 import random
 
+MAX_ATEMPTS = 8
 number = random.randint(1, 100)
 
 def check_guess():
+    if atempts.get() >= MAX_ATEMPTS:
+
+        messagebox.showinfo("Game Over", f"You've reached the max attempts! The number was {number}.")
+        entry.config(state='disabled')
+        check_button.config(state='disabled')
+
+        return
+
     try:
         guess = int(entry.get())
         if guess < number:
@@ -27,16 +37,17 @@ tk.Label(root, text="Guess a number (1-100):").pack()
 entry = tk.Entry(root)
 entry.pack()
 
-tk.Button(root, text="Check", command=check_guess).pack()
+check_button = tk.Button(root, text="Check", command=check_guess)
+check_button.pack()
 
 feedback = tk.StringVar()
 atempts = tk.IntVar()
+
 atempts.set(0)
 formatted_text = tk.StringVar()
 formatted_text.set(f'Atempts: {atempts.get()}')
 
 tk.Label(root, textvariable=feedback).pack()
-
 tk.Label(root, textvariable=formatted_text).pack()
 
 
